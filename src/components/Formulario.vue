0b5bd3a4b2e387215bb1c10a6dd81712b8f256c8
Gabulon <gabo.fajardo7@gmail.com>
2024-01-28 16:47:43 -0600

    Finalizando el formulario

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -25,6 +25,59 @@
                 class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             />
              </div>
+             <div class="mb-5">
+                <label
+                   for="propietario"
+                   class="block text-gray-700 uppercase font-bold">
+                  Nombre Propietario
+               </label>
+              <input
+                  id="propietario"
+                  type="text"
+                  placeholder="Nombre del propietario"
+                  class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              />
+               </div>
+               <div class="mb-5">
+                <label
+                   for="email"
+                   class="block text-gray-700 uppercase font-bold">
+                Email
+               </label>
+              <input
+                  id="email"
+                  type="email"
+                  placeholder="Email del propietario"
+                  class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              />
+               </div>
+               <div class="mb-5">
+                <label
+                   for="alta"
+                   class="block text-gray-700 uppercase font-bold">
+                Alta
+               </label>
+              <input
+                  id="emaialtal"
+                  type="date"
+                  class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+              />
+               </div>
+               <div class="mb-5">
+                <label
+                   for="sintomas"
+                   class="block text-gray-700 uppercase font-bold">
+                Sintomas
+               </label>
+              <textarea
+                  id="sintomas"
+                  placeholder="Describe los sintomas"
+                  class="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md h-48"
+              />
+               </div>
+               <input
+               type="submit"
+               class="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors" value="Registrar Paciente">
         </form>
     </div>
 </template>
